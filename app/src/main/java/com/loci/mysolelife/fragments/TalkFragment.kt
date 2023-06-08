6f9d1c83091e6a4a0abbb689248bc2bc28d5a02e
kdbswo <kdbswo@gmail.com>
2023-06-08 15:12:12 +0900

    Add Load Board list to ListView

--- app/src/main/java/com/loci/mysolelife/fragments/TalkFragment.kt
+++ app/src/main/java/com/loci/mysolelife/fragments/TalkFragment.kt
@@ -2,19 +2,34 @@ package com.loci.mysolelife.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.loci.mysolelife.R
+import com.loci.mysolelife.board.BoardListLVAdapter
+import com.loci.mysolelife.board.BoardModel
 import com.loci.mysolelife.board.BoardWriteActivity
+import com.loci.mysolelife.contentsList.ContentModel
 import com.loci.mysolelife.databinding.FragmentTalkBinding
+import com.loci.mysolelife.utils.FBRef
 
 class TalkFragment : Fragment() {
 
     private lateinit var binding: FragmentTalkBinding
+
+    private val boardDataList = mutableListOf<BoardModel>()
+
+    private val TAG = TalkFragment::class.java.simpleName
+
+    private lateinit var boardLVAdapter: BoardListLVAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +41,10 @@ class TalkFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_talk, container, false)
+
+
+        boardLVAdapter = BoardListLVAdapter(boardDataList)
+        binding.boardListView.adapter = boardLVAdapter
 
         binding.writeBtn.setOnClickListener {
             val intent = Intent(context, BoardWriteActivity::class.java)
@@ -47,9 +66,34 @@ class TalkFragment : Fragment() {
             it.findNavController().navigate(R.id.action_talkFragment_to_storeFragment)
 
         }
-
+        getFBBoardData()
         return binding.root
     }
 
+    private fun getFBBoardData() {
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                boardDataList.clear()
+
+                for (dataModel in dataSnapshot.children) {
+
+                    Log.d(TAG, dataModel.toString())
+                    val item = dataModel.getValue(BoardModel::class.java)
+                    boardDataList.add(item!!)
+
+                }
+                boardLVAdapter.notifyDataSetChanged()
+                Log.d(TAG, boardDataList.toString())
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.boardRef.addValueEventListener(postListener)
+
+    }
 
 }
